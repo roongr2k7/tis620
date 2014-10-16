df65136d1bca604a15a3f8e40e6bb94de91c22aa
Pitsanu Swangpheaw <pitsanu_s@hotmail.com>
2014-10-16 20:14:45 +0700

    contain bytes in struct and change Convert() to UTF8()

--- tis620.go
+++ tis620.go
@@ -1,8 +1,16 @@
 package tis620
 
-func Convert(b []byte) []byte {
+type TIS620 struct {
+	buf []byte
+}
+
+func NewTIS620(b []byte) *TIS620 {
+	return &TIS620{buf: b}
+}
+
+func (t *TIS620) UTF8() []byte {
 	ret := []byte{}
-	for _, v := range b {
+	for _, v := range t.buf {
 		if v > 0xa0 {
 			d := v - 0xa0
 			ret = append(ret, 0xe0, 0xb8|(d>>6), 0x80|(d&0x3f))
--- tis620_test.go
+++ tis620_test.go
@@ -8,7 +8,8 @@ import (
 func TestConvert(t *testing.T) {
 	expectUTF8 := []byte("hello สวัสดี")
 	b := []byte{0x68, 0x65, 0x6c, 0x6c, 0x6f, 0x20, 0xca, 0xc7, 0xd1, 0xca, 0xb4, 0xd5}
-	actual := Convert(b)
+	tis620buf := NewTIS620(b)
+	actual := tis620buf.UTF8()
 	if !reflect.DeepEqual(expectUTF8, actual) {
 		t.Errorf("expect %v but %v", expectUTF8, actual)
 		t.Errorf("expect %q but %q", expectUTF8, actual)
